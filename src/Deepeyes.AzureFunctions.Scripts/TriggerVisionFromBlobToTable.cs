e35812470b491e035aa728d974d1a922f06c3a81
Victor Santele <victor.santele@gmail.com>
2022-08-03 16:07:23 +0200

    export models + add computer vision

--- src/Deepeyes.AzureFunctions.Scripts/TriggerVisionFromBlobToTable.cs
+++ src/Deepeyes.AzureFunctions.Scripts/TriggerVisionFromBlobToTable.cs
@@ -1,28 +1,38 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Deepeyes.Functions.Models;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace Deepeyes.Functions
 {
   public class TriggerVisionFromBlobToTable
   {
-    public class ScanVisionResults
-    {
-      public string PartitionKey { get; set; }
-      public string RowKey { get; set; }
-      public string Image { get; set; }
-      public string[] Tags { get; set; }
-    }
 
     [FunctionName("TriggerVisionFromBlobToTable")]
     [return: Table("ScanVisionResults", Connection = "CosmosDBConnection")]
-    public static ScanVisionResults Run([BlobTrigger("raw-pics/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob, string name, ILogger log)
+    public static async Task<ScanVisionResults> Run([BlobTrigger("raw-pics/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob, string name, ILogger log)
     {
 
       log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
-      return new ScanVisionResults { PartitionKey = "Vision", RowKey = Guid.NewGuid().ToString(), Image = name, Tags = new string[] { "Can", "Coke", "No Sugar" } };
+      // send the blob to vision api and get the results
+      ComputerVisionClient vision = new(new ApiKeyServiceClientCredentials(Environment.GetEnvironmentVariable("ComputerVisionApiKey")), Array.Empty<System.Net.Http.DelegatingHandler>())
+      {
+        Endpoint = Environment.GetEnvironmentVariable("ComputerVisionEndpoint")
+      };
+
+      var results = await vision.DescribeImageInStreamAsync(myBlob);
+      var tags = results.Tags.ToList();
+      var captions = results.Captions.Select(caption => new Caption
+      {
+        Text = caption.Text,
+        Confidence = caption.Confidence
+      }).ToList();
+
+      return new ScanVisionResults { PartitionKey = "Vision", RowKey = Guid.NewGuid().ToString(), Image = name, Tags = tags, Captions = captions };
     }
   }
 }
---
+++ src/Deepeyes.AzureFunctions.Scripts/models/Caption.cs
@@ -0,0 +1,8 @@
+namespace Deepeyes.Functions.Models
+{
+  public class Caption
+  {
+    public string Text { get; set; }
+    public double Confidence { get; set; }
+  }
+}

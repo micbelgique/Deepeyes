37800c74928a2b891112b8550e9d4bbede896d86
Victor Santele <victor.santele@gmail.com>
2022-08-04 10:51:00 +0200

    Migrate to SQL Core API

--- src/Deepeyes.AzureFunctions.Scripts/models/ScanVisionResult.cs
+++ src/Deepeyes.AzureFunctions.Scripts/models/ScanVisionResult.cs
@@ -2,10 +2,9 @@ using System.Collections.Generic;
 
 namespace Deepeyes.Functions.Models
 {
-  public class ScanVisionResults
+  public class ScanVisionResult
   {
-    public string PartitionKey { get; set; }
-    public string RowKey { get; set; }
+    public string Id { get; set; }
     public string Image { get; set; }
     public List<string> Tags { get; set; }
     public List<Caption> Captions { get; set; }

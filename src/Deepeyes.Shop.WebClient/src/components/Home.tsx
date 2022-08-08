917672e532cf8ae507c635836c2ecbcb8a029a0e
Victor Santele <victor.santele@gmail.com>
2022-08-08 15:35:54 +0200

    add search bar

--- src/Deepeyes.Shop.WebClient/src/components/Home.tsx
+++ src/Deepeyes.Shop.WebClient/src/components/Home.tsx
@@ -1,16 +1,18 @@
+import SearchIcon from "@mui/icons-material/Search"
 import Masonry from "@mui/lab/Masonry"
+import { InputAdornment, TextField } from "@mui/material"
 import { Container } from "@mui/system"
 import { useState } from "react"
 import usePollingEffect from "../hooks/usePollingEffect"
 import ScanVisionResult from "../models/ScanVisionResult"
 import ItemCard from "./ItemCard"
 import ItemModal from "./ItemModal"
-
 function Home() {
   const [items, setItems] = useState<ScanVisionResult[]>([])
   const [selectedItem, setSelectedItem] = useState<ScanVisionResult | null>(null)
   const [openModal, setOpenModal] = useState(false)
-  usePollingEffect(
+  const [search, setSearch] = useState("")
+  const { stopPolling, startPolling } = usePollingEffect(
     () => {
       fetch(import.meta.env.VITE_FUNCTION_APP_URL + "/api/readtable")
         .then((res) => res.json())
@@ -22,10 +24,41 @@ function Home() {
     }
   )
 
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (search.length > 0) {
+      stopPolling()
+      const url = new URL("/api/search", import.meta.env.VITE_FUNCTION_APP_URL)
+      const params = new URLSearchParams({ q: search }).toString()
+      fetch(url + "?" + params, {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((data: ScanVisionResult[]) => setItems(data))
+    } else {
+      startPolling()
+    }
+  }
+
   return (
     <div className="Home">
       <h1>Deep Eyes Project</h1>
       <Container>
+        <form onSubmit={handleSearch}>
+          <TextField
+            variant="standard"
+            id="search"
+            label="Search"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
         <Masonry
           columns={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 4 }}
           spacing={2}

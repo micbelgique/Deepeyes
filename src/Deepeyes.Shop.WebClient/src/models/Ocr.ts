b37d88839e5b44686abade53d763fa5effa7e2e0
Victor Santele <victor.santele@gmail.com>
2022-08-10 12:39:51 +0200

    remove useless property

--- src/Deepeyes.Shop.WebClient/src/models/Ocr.ts
+++ src/Deepeyes.Shop.WebClient/src/models/Ocr.ts
@@ -4,7 +4,6 @@ import Summary from "./Summary"
 export default interface Ocr {
   state: string
   lines: string[]
-  summary: string
   entities: Entity[]
   keyPhrases: string[]
   summaries: Summary[]

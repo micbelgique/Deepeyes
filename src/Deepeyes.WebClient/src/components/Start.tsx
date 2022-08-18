88d04ff63a739bc4201bbb48e57ac5e14f2e65e9
Victor Santele <victor.santele@gmail.com>
2022-08-18 14:43:11 +0200

    fix responsive mobile

--- src/Deepeyes.WebClient/src/components/Start.tsx
+++ src/Deepeyes.WebClient/src/components/Start.tsx
@@ -1,30 +1,41 @@
 import CameraIcon from "@mui/icons-material/CameraAlt"
 import ShopIcon from "@mui/icons-material/ShoppingCart"
-import { Card, CardActionArea, CardContent, Stack, Typography } from "@mui/material"
+import { Card, CardActionArea, CardContent, Grid, Typography } from "@mui/material"
 
 export default function App() {
   return (
-    <Stack direction="row" flexWrap="wrap" alignItems="center" justifyContent="center" spacing={4}>
-      <Card sx={{ maxWidth: "18rem" }}>
-        <CardActionArea href="/shop">
-          <ShopIcon sx={{ height: "16rem", width: "16rem" }} />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Shop
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card sx={{ maxWidth: "18rem" }}>
-        <CardActionArea href="/scanner">
-          <CameraIcon sx={{ height: "16rem", width: "16rem" }} />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Scanner
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Stack>
+    <Grid
+      container
+      direction="row"
+      flexWrap="wrap"
+      alignItems="center"
+      justifyContent="center"
+      spacing={4}
+    >
+      <Grid item>
+        <Card sx={{ maxWidth: "18rem" }}>
+          <CardActionArea href="/shop">
+            <ShopIcon sx={{ height: "16rem", width: "16rem" }} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Shop
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item>
+        <Card sx={{ maxWidth: "18rem" }}>
+          <CardActionArea href="/scanner">
+            <CameraIcon sx={{ height: "16rem", width: "16rem" }} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Scanner
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
